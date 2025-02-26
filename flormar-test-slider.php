5eede513c8388fdadcbb4563d088648c3bd148f1
Alex <casperr3848@gmail.com>
2025-02-26 12:45:51 +0200

    Додано фільтрацію товарів за ціною, адаптивність, реалізований дизайн за макетом.

--- flormar-test-slider.php
+++ flormar-test-slider.php
@@ -7,11 +7,11 @@ Author: Alex Stoian
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; 
+    exit;
 }
 
 /**
- * Підключення стилів та скриптів для слайдера
+ * Підключення стилів та скриптів
  */
 function flormar_enqueue_slider_assets() {
     wp_enqueue_style( 'slick-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), '1.8.1' );
@@ -19,23 +19,71 @@ function flormar_enqueue_slider_assets() {
     wp_enqueue_style( 'flormar-slider-css', plugins_url( 'css/style.css', __FILE__ ), array('slick-css'), '1.0' );
 
     wp_enqueue_script( 'slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), '1.8.1', true );
-    wp_enqueue_script( 'flormar-slider-init', plugins_url( 'js/slider-init.js', __FILE__ ), array('jquery','slick-js'), '1.0', true );
+    wp_enqueue_script( 'flormar-slider-init', plugins_url( 'js/slider-init.js', __FILE__ ), array('jquery', 'slick-js'), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'flormar_enqueue_slider_assets' );
 
 /**
- * Реєстрація шорткоду [flormar-test-slider]
+ * Реєстрація шорткоду [flormar-test-slider] з фільтрацією товарів за ціною
  */
-function flormar_test_slider_shortcode() {
-    ob_start();
-    ?>
-    <div class="flormar-slider-wrapper">
-        <h2 class="flormar-slider-title">המוצרים הנמכרים ביותר</h2>
-        <div class="flormar-slider">
-            <!-- Тут будуть товари WooCommerce -->
+function flormar_test_slider_shortcode( $atts ) {
+    $atts = shortcode_atts( array(
+        'min-price' => '',
+        'max-price' => '',
+    ), $atts, 'flormar-test-slider' );
+
+    // Фільтрація товарів за ціною
+    $meta_query = array();
+    if ( ! empty( $atts['min-price'] ) ) {
+        $meta_query[] = array(
+            'key'     => '_price',
+            'value'   => $atts['min-price'],
+            'compare' => '>=',
+            'type'    => 'NUMERIC'
+        );
+    }
+    if ( ! empty( $atts['max-price'] ) ) {
+        $meta_query[] = array(
+            'key'     => '_price',
+            'value'   => $atts['max-price'],
+            'compare' => '<=',
+            'type'    => 'NUMERIC'
+        );
+    }
+
+    $args = array(
+        'post_type'      => 'product',
+        'posts_per_page' => -1,
+    );
+    if ( ! empty( $meta_query ) ) {
+        $args['meta_query'] = $meta_query;
+    }
+
+    $products = new WP_Query( $args );
+
+    if ( $products->have_posts() ) {
+        ob_start();
+        ?>
+        <div class="flormar-slider-wrapper">
+            <h2 class="flormar-slider-title">המוצרים הנמכרים ביותר</h2>
+
+            <div class="flormar-slider">
+                <?php while ( $products->have_posts() ) : $products->the_post(); global $product; ?>
+                    <div class="flormar-slide">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail( 'medium' ); ?>
+                        </a>
+                        <div class="flormar-divider"></div>
+                        <h3 class="flormar-product-title"><?php the_title(); ?></h3>
+                        <p class="flormar-product-price"><?php echo $product->get_price_html(); ?></p>
+                    </div>
+                <?php endwhile; wp_reset_postdata(); ?>
+            </div>
         </div>
-    </div>
-    <?php
-    return ob_get_clean();
+        <?php
+        return ob_get_clean();
+    } else {
+        return '<p>Товари не знайдено.</p>';
+    }
 }
 add_shortcode( 'flormar-test-slider', 'flormar_test_slider_shortcode' );
--- js/slider-init.js
+++ js/slider-init.js
@@ -3,6 +3,17 @@ jQuery(document).ready(function($) {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        arrows: true
+        prevArrow: '<button class="slick-prev slick-arrow"></button>',
+        nextArrow: '<button class="slick-next slick-arrow"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
